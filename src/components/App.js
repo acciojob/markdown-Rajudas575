14f193ba2256ec3f3b4ad24d11f17e27cec2ccdd
Rajudas575 <rajudas575@gmail.com>
2024-12-31 15:36:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
-import './../styles/App.css';
+import './markdown.css'
 
 
 const App = () => {
